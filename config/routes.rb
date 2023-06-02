555ae77ec54471589795d1d39f0244e15999ed56
Simon Hoefling <simon@hoeflia.eu>
2023-06-02 23:27:53 +0800

    Added a user controller with a show method for the user/show view (Profile page) and added the route also

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "categories#index"
   devise_for :users
 
+  resources :users, only: [:show]
   resources :tasks
   resources :categories, except: [:index]
-
 end
