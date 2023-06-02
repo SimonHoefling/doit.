60e8ebed5111167657e10db59ed0f4c1d78b226b
Simon Hoefling <simon@hoeflia.eu>
2023-06-02 15:15:19 +0800

    Added Tasks table to the schema with routes, models, controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_071021) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_071332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
