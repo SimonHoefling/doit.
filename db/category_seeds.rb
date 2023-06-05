b32aa668b081017c974446fe38b970855caab3d3
Simon Hoefling <simon@hoeflia.eu>
2023-06-05 19:21:18 +0800

    Changed seeds and made some links only visible if the admin is logged in

--- db/category_seeds.rb
+++ db/category_seeds.rb
@@ -5,47 +5,58 @@ puts "Creating categories..."
 Category.create([
                   {
                     name: 'Education',
-                    description: 'Test description for the education category'
+                    description: 'Discover a community where learners seek help with their educational challenges,
+                    offering compensation for your expertise.'
                   },
                   {
                     name: 'Moving',
-                    description: 'Test description for the moving category'
+                    description: 'Join a network where individuals share their moving dilemmas, seeking assistance
+                    from reliable helpers in exchange for payment.'
                   },
                   {
                     name: 'Sports',
-                    description: 'Test description for the sports category'
+                    description: 'Be part of a supportive group where people share their fitness goals, searching
+                    for guidance and support in return for compensation.'
                   },
                   {
                     name: 'Handyman',
-                    description: 'Test description for the handyman category'
+                    description: 'Connect with those in need of home repairs and maintenance, offering your skills
+                    to solve their problems for a fair fee.'
                   },
                   {
                     name: 'Beauty',
-                    description: 'Test description for the beauty category'
+                    description: 'Become a trusted resource for individuals looking to enhance their appearance,
+                    providing beauty services for compensation.'
                   },
                   {
                     name: 'PetCare',
-                    description: 'Test description for the pet care category'
+                    description: 'Offer your love and care to pets whose owners seek reliable caregivers, earning
+                    money while nurturing adorable companions.'
                   },
                   {
                     name: 'Babysitting',
-                    description: 'Test description for the babysitting category'
+                    description: 'Provide trustworthy childcare services to busy parents, helping them find peace
+                    of mind in exchange for fair compensation.'
                   },
                   {
                     name: 'Cooking',
-                    description: 'Test description for the cooking category'
+                    description: 'Share your culinary talents with individuals who crave delicious meals, turning
+                    your passion into a rewarding cooking service.'
                   },
                   {
                     name: 'Organizing',
-                    description: 'Test description for the organizing category'
+                    description: 'Assist others in decluttering and organizing their lives, offering your skills
+                    to create harmonious spaces for a reasonable fee.'
                   },
                   {
                     name: 'Cleaning',
-                    description: 'Test description for the cleaning category'
+                    description: 'Help individuals maintain clean and tidy environments, offering your cleaning
+                    services and leaving a spotless impression for compensation.'
                   },
                   {
                     name: 'Events',
-                    description: 'Test description for the events category'
+                    description: 'Be a valuable part of creating unforgettable moments for special occasions,
+                    offering your event planning expertise and earning money in return.'
                   }
                 ])
 
--- db/task_seeds.rb
+++ db/task_seeds.rb
@@ -5,143 +5,213 @@ puts "Creating tasks..."
 Task.create(
   [
     {
-      name: 'Task 1',
-      description: 'Description 1',
-      price: 10.0,
+      name: 'Hairstyle for Special Occasion',
+      description: 'I need a skilled hairstylist to create an elegant hairstyle for a special event.',
+      price: 400_000.0,
       category: Category.fifth,
       user: User.first
     },
     {
-      name: 'Task 2',
-      description: 'Description 2',
-      price: 20.0,
+      name: 'Fixing a Leaky Faucet',
+      description: 'Looking for a reliable handyman to fix a leaky faucet in my kitchen.',
+      price: 220_000.0,
       category: Category.fourth,
       user: User.second
     },
     {
-      name: 'Task 3',
-      description: 'Description 3',
-      price: 20.0,
+      name: 'Personal Fitness Trainer',
+      description: 'Seeking a certified personal fitness trainer to help me achieve my fitness goals.',
+      price: 430_000.0,
       category: Category.third,
       user: User.second
     },
     {
-      name: 'Task 4',
-      description: 'Description 4',
-      price: 24.99,
+      name: 'Furniture Assembly and Disassembly',
+      description: 'Need assistance with assembling and disassembling furniture during a move.',
+      price: 120_000.0,
       category: Category.second,
       user: User.first
     },
     {
-      name: 'Task 5',
-      description: 'Description 5',
-      price: 35.49,
+      name: 'Math Tutor for High School Student',
+      description: 'Searching for a math tutor to help my high school student with algebra.',
+      price: 100_000.0,
       category: Category.first,
       user: User.second
     },
     {
-      name: 'Task 6',
-      description: 'Description 6',
-      price: 22.0,
+      name: 'Makeup Artist for Photoshoot',
+      description: 'Looking for a skilled makeup artist to create stunning looks for a photoshoot.',
+      price: 230_000.0,
       category: Category.fifth,
       user: User.first
     },
     {
-      name: 'Task 7',
-      description: 'Description 7',
-      price: 15.49,
+      name: 'Painting a Room',
+      description: 'Need a reliable handyman to paint a room in my house. ',
+      price: 85_000.00,
       category: Category.fourth,
       user: User.first
     },
     {
-      name: 'Task 8',
-      description: 'Description 8',
-      price: 12.0,
+      name: 'Running Coach',
+      description: 'Seeking a running coach to improve my endurance and speed.',
+      price: 120_000.0,
       category: Category.third,
       user: User.first
     },
     {
-      name: 'Task 9',
-      description: 'Description 9',
-      price: 45.0,
+      name: 'Help with Heavy Lifting',
+      description: 'Looking for strong individuals to assist with heavy lifting during a move.',
+      price: 95_000.0,
       category: Category.second,
       user: User.second
     },
     {
-      name: 'Task 10',
-      description: 'Description 10',
-      price: 32.49,
+      name: 'English Language Tutor',
+      description: 'Seeking an English language tutor to improve my speaking and writing skills.',
+      price: 120_000.00,
       category: Category.first,
       user: User.second
     },
     {
-      name: 'Task 11',
-      description: 'Description 11',
-      price: 18.0,
+      name: 'Manicure and Pedicure Services',
+      description: 'In need of a skilled nail technician for a manicure and pedicure session. ',
+      price: 50_000.0,
       category: Category.fifth,
       user: User.first
     },
     {
-      name: 'Task 12',
-      description: 'Description 12',
-      price: 8.0,
+      name: 'Fixing a Broken Shelf',
+      description: 'Need someone with carpentry skills to fix a broken shelf in my living room.',
+      price: 90_000.0,
       category: Category.fourth,
       user: User.second
     },
     {
-      name: 'Task 13',
-      description: 'Description 13',
-      price: 14.0,
+      name: 'Yoga Instructor for Private Sessions',
+      description: 'Searching for a certified yoga instructor for private yoga sessions.',
+      price: 170_000.0,
       category: Category.third,
       user: User.first
     },
     {
-      name: 'Task 14',
-      description: 'Description 14',
-      price: 17.99,
+      name: 'Packing and Unpacking Assistance',
+      description: 'Looking for helpers to assist with packing and unpacking belongings during a move.',
+      price: 45_000.00,
       category: Category.second,
       user: User.second
     },
     {
-      name: 'Task 15',
-      description: 'Description 15',
-      price: 12.35,
+      name: 'Science Tutor for Middle School Student',
+      description: 'Seeking a science tutor to help my middle school student with biology.',
+      price: 85_000.00,
       category: Category.first,
       user: User.second
     },
     {
-      name: 'Task 16',
-      description: 'Description 16',
-      price: 24.0,
+      name: 'Haircut and Styling for Men',
+      description: 'Need a skilled hairstylist for a haircut and styling session for men.',
+      price: 75_000.0,
       category: Category.fifth,
       user: User.second
     },
     {
-      name: 'Task 17',
-      description: 'Description 17',
-      price: 20.0,
+      name: 'Installing Shelves in a Closet',
+      description: 'Looking for a handyman to install shelves in a closet for better organization.',
+      price: 70_000.0,
       category: Category.fourth,
       user: User.second
     },
     {
-      name: 'Task 18',
-      description: 'Description 18',
-      price: 15.0,
+      name: 'Personal Swimming Instructor',
+      description: 'Seeking a personal swimming instructor to improve my swimming technique.',
+      price: 85_000.0,
       category: Category.third,
       user: User.second
     },
     {
-      name: 'Task 19',
-      description: 'Description 19',
-      price: 42.25,
+      name: 'Furniture Disposal and Recycling',
+      description: 'Need assistance with disposing and recycling old furniture during a move.',
+      price: 40_000.0,
       category: Category.second,
       user: User.second
     },
     {
-      name: 'Task 20',
-      description: 'Description 20',
-      price: 16.49,
+      name: 'History Tutor for College Student',
+      description: 'Searching for a history tutor to help me with my college-level history course.',
+      price: 30_000.00,
       category: Category.first,
+      user: User.second
+    },
+    {
+      name: 'SAT Exam Prep Tutor',
+      description: 'Seeking an experienced tutor to help me prepare for the SAT exam.',
+      price: 140_000.00,
+      category: Category.first,
+      user: User.second
+    },
+    {
+      name: 'Professional Makeup Application',
+      description: 'Looking for a makeup artist to do a professional makeup application for a special occasion.',
+      price: 110_000.0,
+      category: Category.fifth,
+      user: User.first
+    },
+    {
+      name: 'Assembling a Bookshelf',
+      description: 'Need help with assembling a bookshelf and ensuring it is stable.',
+      price: 45_000.0,
+      category: Category.fourth,
+      user: User.second
+    },
+    {
+      name: 'Tennis Coach for Beginners',
+      description: 'Searching for a tennis coach to teach me the basics of playing tennis.',
+      price: 90_000.0,
+      category: Category.third,
+      user: User.first
+    },
+    {
+      name: 'Packing Supplies and Materials Delivery',
+      description: 'Looking for someone to deliver packing supplies and materials for an upcoming move.',
+      price: 25_000.00,
+      category: Category.second,
+      user: User.second
+    },
+    {
+      name: 'Online Math Tutoring for Elementary Student',
+      description: 'Seeking an online math tutor to help my elementary school student with arithmetic.',
+      price: 50_000.00,
+      category: Category.first,
+      user: User.second
+    },
+    {
+      name: 'Bridal Hair and Makeup Services',
+      description: 'Looking for a professional hairstylist and makeup artist for bridal hair and makeup services.',
+      price: 150_000.0,
+      category: Category.fifth,
+      user: User.second
+    },
+    {
+      name: 'Repairing a Leaky Roof',
+      description: 'Need a skilled handyman to repair a leaky roof in my house.',
+      price: 170_000.0,
+      category: Category.fourth,
+      user: User.second
+    },
+    {
+      name: 'Personal Basketball Trainer',
+      description: 'Searching for a personal basketball trainer to improve my basketball skills.',
+      price: 95_000.0,
+      category: Category.third,
+      user: User.second
+    },
+    {
+      name: 'Loading and Unloading Truck',
+      description: 'Looking for individuals to help with loading and unloading items onto a moving truck.',
+      price: 75_000.00,
+      category: Category.second,
       user: User.second
     }
   ]
