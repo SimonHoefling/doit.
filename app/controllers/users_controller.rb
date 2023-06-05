aeef93b0a2f4fe3af74b0e12fb385fbaa3a523d8
Simon Hoefling <simon@hoeflia.eu>
2023-06-05 23:19:27 +0800

    Fixed in user/show page that requested tasks only have to show up in the profile from the user who actually has requested the task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
     @requested_tasks = Task.where(task_status: "requested", user: @user)
     @accepted_tasks = Task.joins(:user).where(users: { id: @user.id }, task_status: "in_work")
     @started_tasks = Task.where("(task_status = 'requested' AND requested_by_id = :user_id) OR task_status = 'in_work'", user_id: @user.id)
+    @task = Task.find_by(requested_by: current_user)
   end
 end
