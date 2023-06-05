1995295343c84587d7b2ae5a3dbb4cb816555653
Simon Hoefling <simon@hoeflia.eu>
2023-06-05 22:42:12 +0800

    Change user login
    Now the user can see category/index and category/show page. As soon as he want to see a task he hast to login or register

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   # GET /categories
   def index
