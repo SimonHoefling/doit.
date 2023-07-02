15500447ea901a01ac867d277ad4d8b0eae90fc6
Simon Hoefling <simon@hoeflia.eu>
2023-07-02 17:45:56 +0700

    Adde read to messages table

---
+++ db/migrate/20230702102418_add_read_to_messages.rb
@@ -0,0 +1,5 @@
+class AddAddressToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :messages, :read, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_104412) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_02_102418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_104412) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read"
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
