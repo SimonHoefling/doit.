8b2443f5c16a5dc536aa90968abc299700bf7975
Simon Hoefling <simon@hoeflia.eu>
2023-07-13 12:41:33 +0800

    Fixed some bugs in the chatroom
    Now if a new message gets send the window adjusts automaticly to the bottom of the chatroom that you can see the message if there are many messages and after closing and reopening the chatroom it automaticliy is at the bottom of the chatroom that you can see the last message

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -8,18 +8,28 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) }
+      { received: data => this.insertMessageAndScrollDown(data) }
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+    this.scrollMessagesToBottom(); // Scroll to the bottom of the chatroom when entering the chatroom
+  }
+
+  insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.scrollMessagesToBottom(); // Scroll to the bottom of the chatroom when a new message is received
+  }
+
+  scrollMessagesToBottom() {
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight; // Function to scroll to the bottom of the chatroom
   }
 
   resetForm(event) {
-    event.target.reset()
+    event.target.reset() // Reset the form once the message is sent
   }
 
   disconnect() {
     console.log("Unsubscribed from the chatroom")
-    this.channel.unsubscribe()
+    this.channel.unsubscribe() // Unsubscribe from the channel when leaving the chatroom
   }
 
 }
