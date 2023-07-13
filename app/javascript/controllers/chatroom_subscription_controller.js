d3d0e4a95cd7ac2c66d8d3c5dd17806e729c2fde
Simon Hoefling <simon@hoeflia.eu>
2023-07-13 17:45:45 +0800

    Added realtime styling for messages in chat

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,25 +4,53 @@ import { createConsumer } from "@rails/actioncable"
 export default class extends Controller {
   static values = { chatroomId: Number }
   static targets = ["messages"]
+  static values = { chatroomId: Number, currentUserId: Number }
+
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.insertMessageAndScrollDown(data) }
+      { received: data => this.insertMessageAndScrollDown(data.message, data.sender_id) }
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
     this.scrollMessagesToBottom(); // Scroll to the bottom of the chatroom when entering the chatroom
   }
 
-  insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.scrollMessagesToBottom(); // Scroll to the bottom of the chatroom when a new message is received
+  insertMessageAndScrollDown(message, sender_id) {
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === sender_id
+
+    // Creating the whole message from the `message` string
+    const messageElement = this.buildMessageElement(currentUserIsSender, message)
+
+    // Inserting the `message` in the DOM
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.scrollMessagesToBottom() // Scroll to the bottom of the chatroom when a new message is received
   }
+
 
   scrollMessagesToBottom() {
     this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight; // Function to scroll to the bottom of the chatroom
   }
+  // jodfa
+  buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.justifyClass(currentUserIsSender)}">
+        <div class="${this.userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
 
+  justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+  // lfdshfd
   resetForm(event) {
     event.target.reset() // Reset the form once the message is sent
   }
