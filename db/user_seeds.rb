18f088e67fbbed91e0831b44cca090f3e1f158f3
Simon Hoefling <simon@hoeflia.eu>
2023-07-24 16:47:49 +0800

    Changed user seeds
    Added a profile avatar to user1 and user2

--- db/user_seeds.rb
+++ db/user_seeds.rb
@@ -34,21 +34,37 @@ User.create([
                 first_name: 'User3',
                 last_name: 'Last_name3',
                 email: 'user3@gmail.com',
-                password: '123456'
+                password: '123456',
+                zip_code: '97463',
+                street_name: 'Downing Street',
+                house_number: '73'
               },
               {
                 first_name: 'User4',
                 last_name: 'Last_name4',
                 email: 'user4@gmail.com',
-                password: '123456'
+                password: '123456',
+                zip_code: '36489',
+                street_name: 'Abbey Road',
+                house_number: '99'
               }
             ])
 
-# I added this to give my user an avatar from the seed file
+# This adds an avatar to the first 3 users
 simon = User.first
 file = File.open(Rails.root.join("app/assets/images/Simon-Avatar.jpg"))
 simon.avatar.attach(io: file, filename: "Simon-Avatar.jpg", content_type: "image/jpg")
 simon.save
+
+user1 = User.second
+file = File.open(Rails.root.join("app/assets/images/user-1.jpg"))
+user1.avatar.attach(io: file, filename: "user-1.jpg", content_type: "image/jpg")
+user1.save
+
+user2 = User.third
+file = File.open(Rails.root.join("app/assets/images/user-2.jpg"))
+user2.avatar.attach(io: file, filename: "user-2.jpg", content_type: "image/jpg")
+user2.save
 
 user_count = User.count
 puts "#{user_count} #{user_count == 1 ? 'user' : 'users'} got created!"
