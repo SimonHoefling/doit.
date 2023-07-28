1d1b1804166a22cad6508986550abc8716863014
Simon Hoefling <simon@hoeflia.eu>
2023-07-28 18:25:15 +0800

    Changed user seeds

--- db/user_seeds.rb
+++ db/user_seeds.rb
@@ -13,36 +13,36 @@ User.create([
                 house_number: '9'
               },
               {
-                first_name: 'User1',
-                last_name: 'Last_name1',
-                email: 'user1@gmail.com',
+                first_name: 'John',
+                last_name: 'Smith',
+                email: 'john@gmail.com',
                 password: '123456',
                 zip_code: '10996',
                 street_name: 'Banana Road',
                 house_number: '14'
               },
               {
-                first_name: 'User2',
-                last_name: 'Last_name2',
-                email: 'user2@gmail.com',
+                first_name: 'Emily',
+                last_name: 'Williams',
+                email: 'emily@gmail.com',
                 password: '123456',
                 zip_code: '10995',
                 street_name: 'Penny Lane',
                 house_number: '45'
               },
               {
-                first_name: 'User3',
-                last_name: 'Last_name3',
-                email: 'user3@gmail.com',
+                first_name: 'Sarah',
+                last_name: 'Brown',
+                email: 'sarah@gmail.com',
                 password: '123456',
                 zip_code: '97463',
                 street_name: 'Downing Street',
                 house_number: '73'
               },
               {
-                first_name: 'User4',
-                last_name: 'Last_name4',
-                email: 'user4@gmail.com',
+                first_name: 'Michael',
+                last_name: 'Jones',
+                email: 'michael@gmail.com',
                 password: '123456',
                 zip_code: '36489',
                 street_name: 'Abbey Road',
@@ -57,14 +57,24 @@ simon.avatar.attach(io: file, filename: "Simon-Avatar.jpg", content_type: "image
 simon.save
 
 user1 = User.second
-file = File.open(Rails.root.join("app/assets/images/user-1.jpg"))
+file = File.open(Rails.root.join("app/assets/images/John-Avatar.jpg"))
 user1.avatar.attach(io: file, filename: "user-1.jpg", content_type: "image/jpg")
 user1.save
 
 user2 = User.third
-file = File.open(Rails.root.join("app/assets/images/user-2.jpg"))
+file = File.open(Rails.root.join("app/assets/images/Emily-Avatar.jpg"))
 user2.avatar.attach(io: file, filename: "user-2.jpg", content_type: "image/jpg")
 user2.save
+
+user3 = User.fourth
+file = File.open(Rails.root.join("app/assets/images/Sarah-Avatar.jpg"))
+user3.avatar.attach(io: file, filename: "user-2.jpg", content_type: "image/jpg")
+user3.save
+
+user4 = User.fifth
+file = File.open(Rails.root.join("app/assets/images/Michael-Avatar.jpg"))
+user4.avatar.attach(io: file, filename: "user-2.jpg", content_type: "image/jpg")
+user4.save
 
 user_count = User.count
 puts "#{user_count} #{user_count == 1 ? 'user' : 'users'} got created!"
